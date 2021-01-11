f2fb78af4c634663f67f326c895ecd0099c9a142
Bas Kuis <b@ukora.com>
2021-01-11 11:54:11 -0600

    spring boot example

---
+++ src/test/java/com/example/simplilearnspringio/SimplilearnspringioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplilearnspringio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplilearnspringioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
